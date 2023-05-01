ccb605bd9d07916da31c91cc29326e5cc74c960b
andrewgspeed03 <112363087+andrewgspeed03@users.noreply.github.com>
2023-04-30 21:30:21 -0500

    Add files via upload
    Imported and Initialize basic GUI components needed

--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,36 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import java.awt.GridBagConstraints;
+/**
+ * 
+ * @author good0161
+ * @version 5.0.1
+ * Designs and creates an executable jar file display the entire trip on a map
+ */
 
 public class Driver {
 	
 	// Declare class data
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
+    	JFrame proFrame = null;
+    	JPanel topPanel = null;
+    	JButton play = null;
+    	JCheckBox enStop = null;
+    	JComboBox aniTime = null;
+    	GridBagConstraints layCont = null;
 
     	// Read file and call stop detection
-    	
+    	TripPoint.readFile("triplog.csv");
+    	TripPoint.h2StopDetection();
     	
     	// Set up frame, include your name in the title
-    	
+    	proFrame = new JFrame("Project 5 - Andrew Goodspeed");
         
         // Set up Panel for input selections
         
