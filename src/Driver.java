c8cea82f4f1ddb01f1d2c83f3ef910e235abcf9f
andrewgspeed03 <112363087+andrewgspeed03@users.noreply.github.com>
2023-05-02 03:18:37 -0500

    Add files via upload
    implement listener for enableStops and attempted to create the animation in the play listener

--- src/Driver.java
+++ src/Driver.java
@@ -1,25 +1,53 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import javax.swing.*;
-import java.awt.*;
-import java.util.*;
-import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import org.openstreetmap.gui.jmapviewer.JMapViewer;
+import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
+import org.openstreetmap.gui.jmapviewer.MapPolygonImpl;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapPolygon;
+import org.openstreetmap.gui.jmapviewer.tilesources.OsmTileSource;
+
 /**
  * 
  * @author good0161
  * @version 5.0.1
- * Designs and creates an executable jar file display the entire trip on a map
+ * Designs and creates an executable jar file display the entire trip on a tripMap
  */
 
 public class Driver {
 	
 	// Declare class data
-	private static JFrame proFrame = null;
-	private static JPanel topPanel = null;
-	private static JButton play = null;
-	private static JCheckBox enStop = null;
-	private static JComboBox<String> aniTime = null;
-	private static GridBagConstraints layCont = null;
+	private static JFrame proFrame;
+	private static JPanel topPanel;
+	private static JButton play;
+	private static JCheckBox enStop;
+	private static JComboBox<String> aniTime;
+	//private static GridBagConstraints layCont;
+	private static JMapViewer tripMap;
+	private static int speed = 15;
+	private static boolean showStops = false;
+	private static Timer timer;
+	private static Image marker = new ImageIcon("raccoon.png").getImage();
 	
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
@@ -27,10 +55,13 @@ public class Driver {
     	TripPoint.readFile("triplog.csv");
     	TripPoint.h2StopDetection();
     	ArrayList<TripPoint> Trip = TripPoint.getTrip();
+    	ArrayList<TripPoint> movingTrip = TripPoint.getMovingTrip();
     	
     	// Set up frame, include your name in the title
     	proFrame = new JFrame("Project 5 - Andrew Goodspeed");
         proFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        proFrame.setSize(400, 400);
+        proFrame.setLayout(new BorderLayout());
         
         // Set up Panel for input selections
         topPanel = new JPanel();
@@ -52,21 +83,24 @@ public class Driver {
         topPanel.add(play);
         topPanel.add(enStop);
         topPanel.add(aniTime);
+        proFrame.add(topPanel, BorderLayout.NORTH);
         
-        // Set up mapViewer
-        
+        // Set up tripMap
+        tripMap = new JMapViewer();
+        tripMap.setTileSource(new OsmTileSource.TransportMap());
+        proFrame.add(tripMap, BorderLayout.CENTER);
         
         // Add listeners for GUI components
         play.addActionListener(new ActionListener() {
         	@Override
         	public void actionPerformed(ActionEvent e) {
-        		
+        		Play(Trip);
         	}
         });
         enStop.addActionListener(new ActionListener() {
         	@Override
         	public void actionPerformed(ActionEvent e) {
-        		
+        		showStops = !showStops;
         	}
         });
         aniTime.addItemListener(new ItemListener() {
@@ -75,17 +109,44 @@ public class Driver {
         		if(e.getStateChange() == ItemEvent.SELECTED) {
         			String sel = (String) aniTime.getSelectedItem();
         			if(aniTime.getSelectedIndex() != 0) {
-        				int speed = Integer.parseInt(sel);
-        				// divide Animation Time by speed
+        				speed = Integer.parseInt(sel);
         			}        			
         		}
         	}
         });
-
-        // Set the map center and zoom level
         
+
+        // Set the tripMap center and zoom level
+        proFrame.setVisible(true);
+    }
+    // Animate the trip based on selections from the GUI components
+
+
+    private static void Play(ArrayList<TripPoint> Trip){
+
+    	tripMap.removeAllMapPolygons();
+    	List<MapMarkerDot> points = new ArrayList<MapMarkerDot>();
+    	
+    	for(TripPoint x: Trip) {
+    		points.add(new MapMarkerDot(x.getLat(),x.getLon()));	
+    	}
+    	int duration = (Trip.size() / speed); 
+    	timer = new Timer(duration, new ActionListener() {
+    			@Override
+    			public void actionPerformed(ActionEvent e) {
+    				for(int i = 0; i < points.size()-1; i++) {
+    					MapMarkerDot from = points.get(i);
+    					MapMarkerDot to = points.get(i + 1);
+    		    		MapPolygon line = new MapPolygonImpl(from,from,to);
+    		    		tripMap.addMapPolygon(line);
+    		    		
+    		    	}
+    			}
+    	});
+    	
     }
     
-    // Animate the trip based on selections from the GUI components
     
+    
+ 
 }
