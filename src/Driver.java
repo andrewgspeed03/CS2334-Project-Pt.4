46f8d6462b81fcabf0c3ee6c02f420ccc4121e32
andrewgspeed03 <112363087+andrewgspeed03@users.noreply.github.com>
2023-05-02 17:12:52 -0500

    Completed raccoon animation

--- src/Driver.java
+++ src/Driver.java
@@ -18,13 +18,22 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayer;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
+import org.openstreetmap.gui.jmapviewer.Layer;
 import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
 import org.openstreetmap.gui.jmapviewer.MapPolygonImpl;
+import org.openstreetmap.gui.jmapviewer.MapObjectImpl;
+import org.openstreetmap.gui.jmapviewer.events.JMVCommandEvent;
+import org.openstreetmap.gui.jmapviewer.interfaces.JMapViewerEventListener;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapObject;
 import org.openstreetmap.gui.jmapviewer.interfaces.MapPolygon;
+import org.openstreetmap.gui.jmapviewer.interfaces.TileLoader;
+import org.openstreetmap.gui.jmapviewer.interfaces.TileSource;
 import org.openstreetmap.gui.jmapviewer.tilesources.OsmTileSource;
 
 /**
@@ -47,22 +56,26 @@ public class Driver {
 	private static int speed = 15;
 	private static boolean showStops = false;
 	private static Timer timer;
-	private static Image marker = new ImageIcon("raccoon.png").getImage();
+	private static final Image marker = new ImageIcon("raccoon.png").getImage();
+	private static float progress = 0;
+	private static int numStops;
+
 	
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
     	// Read file and call stop detection
     	TripPoint.readFile("triplog.csv");
-    	TripPoint.h2StopDetection();
-    	ArrayList<TripPoint> Trip = TripPoint.getTrip();
-    	ArrayList<TripPoint> movingTrip = TripPoint.getMovingTrip();
+    	numStops = TripPoint.h2StopDetection();
+    	ArrayList<TripPoint> trip = TripPoint.getTrip();
+    	ArrayList<TripPoint> stops = new ArrayList<>(trip);
+    	stops.removeAll(TripPoint.getMovingTrip());
     	
     	// Set up frame, include your name in the title
     	proFrame = new JFrame("Project 5 - Andrew Goodspeed");
         proFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         proFrame.setSize(400, 400);
         proFrame.setLayout(new BorderLayout());
-        
+    
         // Set up Panel for input selections
         topPanel = new JPanel();
     	
@@ -89,20 +102,23 @@ public class Driver {
         tripMap = new JMapViewer();
         tripMap.setTileSource(new OsmTileSource.TransportMap());
         proFrame.add(tripMap, BorderLayout.CENTER);
-        
+
         // Add listeners for GUI components
         play.addActionListener(new ActionListener() {
         	@Override
         	public void actionPerformed(ActionEvent e) {
         		if(timer != null) 
         			timer.stop();
-        		Play(Trip);
+        		Play(trip,stops);
         	}
         });
         enStop.addActionListener(new ActionListener() {
         	@Override
         	public void actionPerformed(ActionEvent e) {
-        		showStops = !showStops;
+        		if(enStop.isSelected())
+        			showStops = true;
+        		else
+        			showStops = false;
         	}
         });
         aniTime.addItemListener(new ItemListener() {
@@ -124,37 +140,62 @@ public class Driver {
     // Animate the trip based on selections from the GUI components
 
 
-    private static void Play(ArrayList<TripPoint> Trip){
+    private static void Play(ArrayList<TripPoint> Trip, ArrayList<TripPoint> Stops){
     	
+    	tripMap.removeAllMapMarkers();
     	tripMap.removeAllMapPolygons();
-    	List<MapMarkerDot> points = new ArrayList<MapMarkerDot>();
     	
-    	for(TripPoint x: Trip) {
-    		points.add(new MapMarkerDot(x.getLat(),x.getLon()));	
-    	}
-    	int duration = (Trip.size() / speed); 
+    	List<MapMarkerDot> line = new ArrayList<MapMarkerDot>();
+    	List<MapMarkerDot> dot = new ArrayList<MapMarkerDot>();
+    	
+    	Graphics g = (Graphics2D) proFrame.getGraphics();
+    	g.setColor(Color.RED);
+    	
+    	for(TripPoint x: Trip) 
+    		line.add(new MapMarkerDot(x.getLat(),x.getLon()));	
+    	for(TripPoint y: Stops)
+    		dot.add(new MapMarkerDot(y.getLat(),y.getLon()));
     	final int[] current = {0};
-    	
+    	final int[] stopCount = {0};
+    	final MapMarker[] prev = new MapMarker[1];
+    	int duration = ((speed * 1000) / Trip.size());
+    	duration += ((speed * 1000) % Trip.size());
     	
     	timer = new Timer(duration, new ActionListener() {
     			@Override
     			public void actionPerformed(ActionEvent e) {
-    				if(current[0] != points.size()-1) {
-	    				MapMarkerDot from = points.get(current[0]);
-	    				MapMarkerDot to = points.get(current[0] + 1);
-	    		    	MapPolygon line = new MapPolygonImpl(from,to,to);
-	    		    
+    				
+    				if(current[0] != line.size()-1) {
+	    				MapMarkerDot from = line.get(current[0]);
+	    				MapMarkerDot to = line.get(current[0] + 1);
+	    				
+	    		    	MapPolygonImpl line = new MapPolygonImpl(from,from ,to);
+	    		    	MapMarker rac = new IconMarker(to.getCoordinate(), marker);
+	    		    	
+	    		    	line.setColor(Color.RED);
+	    		    	tripMap.removeMapMarker(prev[0]);
+	    		    	tripMap.addMapMarker(rac);
 	    		    	tripMap.addMapPolygon(line);
+	    		    	prev[0] = rac;
+	    		    	if(showStops && stopCount[0] < numStops) { 
+	    		    		MapMarkerDot stop = dot.get(stopCount[0]);
+	    		    		stop.setBackColor(Color.RED);
+	    		    		stop.setColor(Color.RED);
+	    		    		if(to.getCoordinate().equals(stop.getCoordinate())){
+	    		    			tripMap.addMapMarker(dot.get(stopCount[0]));
+	    		    			stopCount[0]++;
+	    		    		}
+	    		    	}
     				}
     				else
     					timer.stop();
+    				
     				current[0]++;
+    				
     			}
     	});
     	timer.start();
     }
-    
-    
-    
- 
+   
+   
 }
