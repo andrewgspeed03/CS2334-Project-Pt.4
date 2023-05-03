833d20f7718e735593079bf6d4b20fa652e9ad52
andrewgspeed03 <112363087+andrewgspeed03@users.noreply.github.com>
2023-05-03 01:44:46 -0500

    Completed animation only issue is the speed

--- src/Driver.java
+++ src/Driver.java
@@ -17,23 +17,14 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JLayer;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
-import org.openstreetmap.gui.jmapviewer.Layer;
 import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
 import org.openstreetmap.gui.jmapviewer.MapPolygonImpl;
-import org.openstreetmap.gui.jmapviewer.MapObjectImpl;
-import org.openstreetmap.gui.jmapviewer.events.JMVCommandEvent;
-import org.openstreetmap.gui.jmapviewer.interfaces.JMapViewerEventListener;
 import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
-import org.openstreetmap.gui.jmapviewer.interfaces.MapObject;
-import org.openstreetmap.gui.jmapviewer.interfaces.MapPolygon;
-import org.openstreetmap.gui.jmapviewer.interfaces.TileLoader;
-import org.openstreetmap.gui.jmapviewer.interfaces.TileSource;
 import org.openstreetmap.gui.jmapviewer.tilesources.OsmTileSource;
 
 /**
@@ -57,7 +48,6 @@ public class Driver {
 	private static boolean showStops = false;
 	private static Timer timer;
 	private static final Image marker = new ImageIcon("raccoon.png").getImage();
-	private static float progress = 0;
 	private static int numStops;
 
 	
@@ -73,7 +63,7 @@ public class Driver {
     	// Set up frame, include your name in the title
     	proFrame = new JFrame("Project 5 - Andrew Goodspeed");
         proFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        proFrame.setSize(400, 400);
+        proFrame.setSize(1920, 1080);
         proFrame.setLayout(new BorderLayout());
     
         // Set up Panel for input selections
@@ -101,15 +91,25 @@ public class Driver {
         // Set up tripMap
         tripMap = new JMapViewer();
         tripMap.setTileSource(new OsmTileSource.TransportMap());
+        
         proFrame.add(tripMap, BorderLayout.CENTER);
-
+        
         // Add listeners for GUI components
         play.addActionListener(new ActionListener() {
         	@Override
         	public void actionPerformed(ActionEvent e) {
         		if(timer != null) 
         			timer.stop();
-        		Play(trip,stops);
+        		try {
+					Play(trip,stops);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+        		
         	}
         });
         enStop.addActionListener(new ActionListener() {
@@ -135,42 +135,45 @@ public class Driver {
         
 
         // Set the tripMap center and zoom level
+        tripMap.setDisplayPosition(new Coordinate(35,-110), 6);
         proFrame.setVisible(true);
     }
     // Animate the trip based on selections from the GUI components
 
 
-    private static void Play(ArrayList<TripPoint> Trip, ArrayList<TripPoint> Stops){
+    private static void Play(ArrayList<TripPoint> Trip, ArrayList<TripPoint> Stops) throws FileNotFoundException, IOException{
     	
     	tripMap.removeAllMapMarkers();
     	tripMap.removeAllMapPolygons();
     	
-    	List<MapMarkerDot> line = new ArrayList<MapMarkerDot>();
-    	List<MapMarkerDot> dot = new ArrayList<MapMarkerDot>();
+    	List<Coordinate> line = new ArrayList<Coordinate>();
+    	List<Coordinate> dot = new ArrayList<Coordinate>();
     	
+
     	Graphics g = (Graphics2D) proFrame.getGraphics();
     	g.setColor(Color.RED);
+    	int period = 1000;
     	
     	for(TripPoint x: Trip) 
-    		line.add(new MapMarkerDot(x.getLat(),x.getLon()));	
+    		line.add(new Coordinate(x.getLat(),x.getLon()));	
     	for(TripPoint y: Stops)
-    		dot.add(new MapMarkerDot(y.getLat(),y.getLon()));
+    		dot.add(new Coordinate(y.getLat(),y.getLon()));
+    	
     	final int[] current = {0};
     	final int[] stopCount = {0};
     	final MapMarker[] prev = new MapMarker[1];
-    	int duration = ((speed * 1000) / Trip.size());
-    	duration += ((speed * 1000) % Trip.size());
-    	
-    	timer = new Timer(duration, new ActionListener() {
+    	period =  (speed * 1000) / line.size();
+
+    	timer = new Timer(period, new ActionListener() {
     			@Override
     			public void actionPerformed(ActionEvent e) {
     				
-    				if(current[0] != line.size()-1) {
-	    				MapMarkerDot from = line.get(current[0]);
-	    				MapMarkerDot to = line.get(current[0] + 1);
+    				if(current[0] < line.size()-1) {
+    					Coordinate from = line.get(current[0]);
+    					Coordinate to = line.get(current[0] + 1);
 	    				
 	    		    	MapPolygonImpl line = new MapPolygonImpl(from,from ,to);
-	    		    	MapMarker rac = new IconMarker(to.getCoordinate(), marker);
+	    		    	MapMarker rac = new IconMarker(to , marker);
 	    		    	
 	    		    	line.setColor(Color.RED);
 	    		    	tripMap.removeMapMarker(prev[0]);
@@ -178,22 +181,24 @@ public class Driver {
 	    		    	tripMap.addMapPolygon(line);
 	    		    	prev[0] = rac;
 	    		    	if(showStops && stopCount[0] < numStops) { 
-	    		    		MapMarkerDot stop = dot.get(stopCount[0]);
-	    		    		stop.setBackColor(Color.RED);
-	    		    		stop.setColor(Color.RED);
-	    		    		if(to.getCoordinate().equals(stop.getCoordinate())){
-	    		    			tripMap.addMapMarker(dot.get(stopCount[0]));
+	    		    		Coordinate stop = dot.get(stopCount[0]);
+	    		    		if(to.equals(stop)){
+	    		    			MapMarkerDot stopMark = new MapMarkerDot(stop);
+	    		    			stopMark.setBackColor(Color.RED);
+	    		    			stopMark.setColor(Color.RED);
+	    		    			
+	    		    			tripMap.addMapMarker(stopMark);
 	    		    			stopCount[0]++;
 	    		    		}
 	    		    	}
+	    		    	
     				}
-    				else
-    					timer.stop();
     				
     				current[0]++;
     				
     			}
     	});
+    	timer.setInitialDelay(0);
     	timer.start();
     }
    
